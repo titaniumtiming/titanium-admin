438da860710135cc443bfd7a4abcc4455a9ff9d0
DawidWraga <dpwraga@gmail.com>
2024-03-13 16:46:07 +0000

    feat: create bulk insert utils

--- src/services/configure-services.ts
+++ src/services/configure-services.ts
@@ -6,6 +6,9 @@ import { createPool } from "mysql2/promise";
 export type ServiceContext = {
   mssqlRacetecDb: mssql.ConnectionPool;
   mysqlRemoteDb: ReturnType<typeof createPool>;
+  bulkInsertIntoRemoteDb: <T>(
+    options: BulkInsertOrUpdateOptions<T>,
+  ) => Promise<unknown>;
 };
 
 // Declare variables to hold the singleton instances.
@@ -47,5 +50,67 @@ export const createServiceContext = async (): Promise<ServiceContext> => {
   return {
     mssqlRacetecDb,
     mysqlRemoteDb,
+    bulkInsertIntoRemoteDb: async (options: BulkInsertOrUpdateOptions<any>) => {
+      return bulkInsertOrUpdate(options, async (sql, params) => {
+        const [result] = await mysqlRemoteDb!.execute(sql, params);
+        return result;
+      });
+    },
   };
 };
+
+type BulkInsertOrUpdateOptions<T> = {
+  tableName: string;
+  data: T[];
+  updateOnDuplicate: boolean;
+  logSql?: boolean;
+  logParams?: boolean;
+};
+
+export async function bulkInsertOrUpdate<T extends Record<string, any>>(
+  options: BulkInsertOrUpdateOptions<T>,
+  executeQuery: (sql: string, params: Record<string, any>) => Promise<any>,
+) {
+  const { tableName, data, updateOnDuplicate } = options;
+
+  if (data.length === 0) return; // Handle empty data case
+
+  // Define the columns based on the keys of the first object in the array
+  // @ts-expect-error
+  const columns = Object.keys(data[0]);
+  const placeholders = data
+    .map(
+      (_, index) =>
+        `(${columns.map((column) => `:${column}${index}`).join(",")})`,
+    )
+    .join(",");
+
+  const sqlBase = `
+    INSERT INTO \`${tableName}\` (${columns.join(", ")})
+    VALUES ${placeholders}
+  `;
+
+  const sqlUpdateOnDuplicate = updateOnDuplicate
+    ? `
+    ON DUPLICATE KEY UPDATE
+    ${columns.map((column) => `${column} = VALUES(${column})`).join(", ")}
+  `
+    : "";
+
+  const sql = sqlBase + sqlUpdateOnDuplicate;
+
+  // Map the array of objects to a single object with unique keys for each placeholder
+  const namedParameters = data.reduce((acc, currentItem, index) => {
+    columns.forEach((column) => {
+      // @ts-expect-error
+      acc[`${column}${index}`] = currentItem[column];
+    });
+    return acc;
+  }, {});
+
+  options.logSql && console.log("sql: ", sql);
+  options.logParams && console.log("params: ", namedParameters);
+
+  // Execute the SQL query with the constructed SQL statement and parameters
+  return executeQuery(sql, namedParameters);
+}
