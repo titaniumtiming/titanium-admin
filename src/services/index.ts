ad4fd5d52853c8e590f9ffe4d475a907d693f92e
DawidWraga <dpwraga@gmail.com>
2024-03-19 17:09:41 +0000

    feat: sync all

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,26 +8,29 @@ import { syncAll } from "@/services/sync-all";
 import { syncEvents } from "@/services/sync/sync-events";
 import { syncRaces } from "@/services/sync/sync-races";
 import * as pingServices from "@/services/ping";
+import * as countServices from "@/services/count";
 import { syncPrimaryCategories } from "@/services/sync/sync-primary-category";
 import { syncSecondaryCategories } from "@/services/sync/sync-secondary-categories";
 import { syncAthletes } from "@/services/sync/sync-athletes";
 import { syncSplits } from "@/services/sync/sync-splits";
 import { syncAthleteResults } from "@/services/sync/sync-athlete-results";
+import { syncAthleteSplits } from "@/services/sync/sync-athlete-splits";
 export const syncServices = {
   Races: syncRaces,
   Events: syncEvents,
   PrimaryCategories: syncPrimaryCategories,
   SecondaryCategories: syncSecondaryCategories,
   AthleteResults: syncAthleteResults,
-  AthleteSplits: syncRaces,
+  AthleteSplits: syncAthleteSplits,
   Athletes: syncAthletes,
   Splits: syncSplits,
 } satisfies Record<SyncDbTableName, unknown>;
 
 export const serviceRouter = createTRPCRouter({
-  sync: createServiceRouter(syncServices),
   syncAll: createTRPCProcedure(syncAll),
+  sync: createServiceRouter(syncServices),
   ping: createServiceRouter(pingServices),
+  count: createServiceRouter(countServices),
 });
 
 export type ServiceRouter = typeof serviceRouter;
--- src/services/sync-all.ts
+++ src/services/sync-all.ts
@@ -2,9 +2,13 @@ import { service } from "@/lib/service";
 import { syncServices } from "@/services";
 
 export const syncAll = service().mutation(async ({ ctx }) => {
-  await Promise.all(
-    Object.entries(syncServices).map(([tableName, syncService]) =>
-      syncService(ctx),
-    ),
-  );
+  // If use Promise.all get errors (too many connections) so must run sequentially
+
+  const results = [] as any[];
+  for (const [tableName, syncService] of Object.entries(syncServices)) {
+    const result = await syncService(ctx);
+    results.push([tableName, result]);
+  }
+
+  return results;
 });
